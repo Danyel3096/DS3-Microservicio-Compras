a9e39d9c62021ed587f8621d8957daacbade5282
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-03 13:55:29 -0500

    Feat: Configuring order controller
    Adding some new methods as endpoints of Frontend petitions

--- src/main/java/com/ds3/team8/orders_service/controllers/OrderController.java
+++ src/main/java/com/ds3/team8/orders_service/controllers/OrderController.java
@@ -1,4 +1,73 @@
 package com.ds3.team8.orders_service.controllers;
 
+import com.ds3.team8.orders_service.dtos.OrderRequest;
+import com.ds3.team8.orders_service.dtos.OrderResponse;
+import com.ds3.team8.orders_service.services.IOrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@RestController // Indica que esta clase es un controlador REST
+@RequestMapping("/api/v1/orders") // Indica la URL base para acceder a los servicios de esta clase
+
 public class OrderController {
+
+    private IOrderService orderService;
+
+    public OrderController(IOrderService orderService){
+        this.orderService = orderService;
+    }
+
+    // Obtener todos las órdenes
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    // Crear una órden
+    @PostMapping
+    public ResponseEntity<OrderResponse> saveOrder(@Valid @RequestBody OrderRequest orderRequest) {
+        OrderResponse savedOrder = orderService.save(orderRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedOrder);
+    }
+
+    // Actualizar una órden
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrder(
+            @PathVariable Long id,
+            @Valid @RequestBody OrderRequest orderRequest) {
+        OrderResponse updatedOrder = orderService.update(id, orderRequest);
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    // Buscar órdens con paginación
+    // Cambia la URL a algo como /api/v1/orders/pageable?page=0&size=10&sort=firstName,asc
+    // Para el sort= se puede usar cualquier atributo de la entidad Order
+    // Por ejemplo para ordenar por apellido desde la Z a la A sería sort=lastName,desc
+    // Para el caso de roles se puede usar sort=role.name,asc (sort=entity.attribute,asc o desc)
+    @GetMapping("/pageable")
+    public Page<OrderResponse> findAllPageable(Pageable pageable) {
+        return orderService.findAllPageable(pageable);
+    }
+
+
+    // Obtener una órden por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.findById(id));
+    }
+
+    // Eliminación lógica de una órden
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        orderService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
