bebaba2343eccb47434a76905306361ef5a2d985
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-05 00:14:54 -0500

    Feat: Configuring order and order item
    Putting attributes according the database tables name

--- src/main/java/com/ds3/team8/orders_service/entities/Order.java
+++ src/main/java/com/ds3/team8/orders_service/entities/Order.java
@@ -1,4 +1,32 @@
 package com.ds3.team8.orders_service.entities;
 
+import java.sql.Date;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // Genera automáticamente getters, setters, equals, hashCode y toString
+@NoArgsConstructor  // Constructor sin argumentos
+@AllArgsConstructor // Constructor con todos los argumentos
+@Entity  // Indica que esta clase es una entidad JPA
+@Table(name = "orders")  // Nombre de la tabla en la base de datos
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Autoincremental
+    private Long id;
+
+    @Column(nullable = false)
+    private Long user_id; // ID del usuario que realizó la orden
+
+    @Column(nullable = false)
+    private Date order_date; // Número de telefono del usuario
+
+    @Column(nullable = false)
+    private String status; // Estado de la orden (pendiente, en proceso, completada, cancelada)
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
 }
--- src/main/java/com/ds3/team8/orders_service/entities/OrderItem.java
+++ src/main/java/com/ds3/team8/orders_service/entities/OrderItem.java
@@ -1,4 +1,30 @@
 package com.ds3.team8.orders_service.entities;
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // Genera automáticamente getters, setters, equals, hashCode y toString
+@NoArgsConstructor  // Constructor sin argumentos
+@AllArgsConstructor // Constructor con todos los argumentos
+@Entity  // Indica que esta clase es una entidad JPA
+@Table(name = "order_items")  // Nombre de la tabla en la base de datos
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Autoincremental
+    private Long id;
+
+    @Column(nullable = false)
+    private Long order_id; // ID de la orden a la que pertenece el item
+
+    @Column(nullable = false)
+    private Long product_id; // Identificador del producto
+
+    @Column(nullable = false)
+    private Long quantity; // Cantidad del producto (stock)
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
 }
