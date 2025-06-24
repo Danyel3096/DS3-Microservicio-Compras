3f086bd6dd85bb1850bde45a0d0cbad3dcb503b0
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-23 22:54:38 -0500

    Feat: Updating Order Item Controller file

--- src/main/java/com/ds3/team8/orders_service/controllers/OrderItemController.java
+++ src/main/java/com/ds3/team8/orders_service/controllers/OrderItemController.java
@@ -1,12 +1,16 @@
 package com.ds3.team8.orders_service.controllers;
 
-import com.ds3.team8.orders_service.dtos.OrderItemRequest;
+import com.ds3.team8.orders_service.client.enums.Role;
 import com.ds3.team8.orders_service.dtos.OrderItemResponse;
 import com.ds3.team8.orders_service.services.IOrderItemService;
-import jakarta.validation.Valid;
+import com.ds3.team8.orders_service.utils.SecurityUtil;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/order-items")
+@Tag(name = "Ítems de Pedido", description = "Endpoints para ítems de pedido")
 public class OrderItemController {
 
     private final IOrderItemService orderItemService;
@@ -23,48 +28,56 @@ public class OrderItemController {
     }
 
     // Obtener todos los ítems
+    @Operation(summary = "Obtener todos los ítems", description = "Obtener todos los ítems de pedido del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @GetMapping
-    public ResponseEntity<List<OrderItemResponse>> getAllOrderItems() {
+    public ResponseEntity<List<OrderItemResponse>> getAllOrderItems(
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         return ResponseEntity.ok(orderItemService.findAll());
     }
 
-    // Crear un ítem de orden
-    @PostMapping
-    public ResponseEntity<OrderItemResponse> saveOrderItem(@Valid @RequestBody OrderItemRequest orderItemRequest) {
-        OrderItemResponse savedItem = orderItemService.save(orderItemRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedItem);
-    }
-
-    // Actualizar ítem
-    @PutMapping("/{id}")
-    public ResponseEntity<OrderItemResponse> updateOrderItem(
-            @PathVariable Long id,
-            @Valid @RequestBody OrderItemRequest orderItemRequest) {
-        OrderItemResponse updatedItem = orderItemService.update(id, orderItemRequest);
-        return ResponseEntity.ok(updatedItem);
-    }
-
     // Obtener ítem por ID
+    @Operation(summary = "Obtener un ítem de orden por su id", description = "Obtener un ítem de pedido por su id del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @GetMapping("/{id}")
     public ResponseEntity<OrderItemResponse> getOrderItemById(@PathVariable Long id) {
         return ResponseEntity.ok(orderItemService.findById(id));
     }
 
-    // Eliminar ítem lógicamente
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteOrderItem(@PathVariable Long id) {
-        orderItemService.delete(id);
-        return ResponseEntity.noContent().build();
+    // Obtener ítems paginados
+    // Ejemplo URL /api/v1/order-items/pageable?page=0&size=8
+    @Operation(summary = "Obtener ítems de orden paginados", description = "Obtener ítems de pedido paginados del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
+    @GetMapping("/pageable")
+    public Page<OrderItemResponse> getOrderItemsPageable(
+        Pageable pageable,
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
+        return orderItemService.findAllPageable(pageable);
     }
 
-    // Obtener ítems paginados
-    // Obtener órdenes paginadas
-    // Cambia la URL a algo como /api/v1/order-items/pageable?page=0&size=10&sort=productId,desc
-    // Para el sort= se puede usar cualquier atributo de la entidad User
-    // Por ejemplo para ordenar por apellido desde la Z a la A sería sort=lastName,desc
-    // Para el caso de roles se puede usar sort=role.name,asc (sort=entity.attribute,asc o desc)
-    @GetMapping("/pageable")
-    public Page<OrderItemResponse> getOrderItemsPageable(Pageable pageable) {
-        return orderItemService.findAllPageable(pageable);
+    // Obtener ítems por ID de orden
+    @Operation(summary = "Obtener ítems de orden por ID de orden", description = "Obtener ítems de pedido por ID de orden del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<List<OrderItemResponse>> getOrderItemsByOrderId(@PathVariable Long orderId) {
+        List<OrderItemResponse> items = orderItemService.findAllByOrderId(orderId);
+        return ResponseEntity.ok(items);
+    }
+
+    // Obtener ítems por ID de orden paginados
+    // Ejemplo URL /api/v1/order-items/order/1/pageable?page=0&size=8
+    @Operation(summary = "Obtener ítems de orden por ID de orden paginados", description = "Obtener ítems de pedido por ID de orden paginados del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
+    @GetMapping("/order/{orderId}/pageable")
+    public Page<OrderItemResponse> getOrderItemsByOrderIdPageable(
+            @PathVariable Long orderId,
+            Pageable pageable) {
+        return orderItemService.findAllByOrderIdPageable(orderId, pageable);
+    }
+
+    // Verificar si existe un ítem de orden por ID de producto
+    @Hidden
+    @GetMapping("/product/{productId}/exists")
+    public Boolean orderItemHasProducts(@PathVariable Long productId) {
+        return orderItemService.orderItemHasProducts(productId);
     }
 }
