a0926929976b38088a32bed85154526a972d4d74
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-23 22:57:55 -0500

    Feat: Updating Order Request file

--- src/main/java/com/ds3/team8/orders_service/dtos/OrderRequest.java
+++ src/main/java/com/ds3/team8/orders_service/dtos/OrderRequest.java
@@ -1,13 +1,11 @@
 package com.ds3.team8.orders_service.dtos;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -15,17 +13,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequest {
-
-    @NotNull(message = "El ID del usuario es obligatorio")
-    private Long userId;
-
-    @NotNull(message = "El estado de la orden es obligatorio")
-    @Size(min = 3, max = 50, message = "El estado debe tener entre 3 y 50 caracteres")
-    private String status;
-
-    // Lista de items que se van a incluir en la orden
+    @NotNull(message = "El campos 'items' es obligatorio")
     private List<OrderItemRequest> items;
-
-    // Fecha opcionalmente enviada desde el cliente
-    private LocalDateTime orderDate;
 }
