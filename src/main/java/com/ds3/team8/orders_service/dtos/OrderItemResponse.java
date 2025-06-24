4622ae69871c74bce7137f2565a7519393510704
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-23 22:56:40 -0500

    Feat: Updating Order Item Response file

--- src/main/java/com/ds3/team8/orders_service/dtos/OrderItemResponse.java
+++ src/main/java/com/ds3/team8/orders_service/dtos/OrderItemResponse.java
@@ -1,5 +1,7 @@
 package com.ds3.team8.orders_service.dtos;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,7 @@ public class OrderItemResponse {
     private Long id;
     private Long orderId;
     private Long productId;
-    private Long quantity;
-    private Boolean isActive;
+    private Integer quantity;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
