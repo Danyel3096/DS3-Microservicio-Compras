e117b96730e4348e112f9fb7f00d7fae74e78a90
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-23 22:48:00 -0500

    Feat: Creating dtos folder to save client functionalities

---
+++ src/main/java/com/ds3/team8/orders_service/client/dtos/ProductResponse.java
@@ -0,0 +1,24 @@
+package com.ds3.team8.orders_service.client.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private Long categoryId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
